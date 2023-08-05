9600b8737f435b3da705a1dd211aa18f0bc5d349
Julian Vogel <birdman.coding.1991@gmail.com>
2023-08-05 10:42:27 +0200

    backend route room

--- app/api/room/route.ts
+++ app/api/room/route.ts
@@ -1,11 +1,20 @@
 import { withValidation } from "@/lib/withValidation";
 import { RoomRequest, RoomRequestSchema } from "@/schemas/roomSchema";
 import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
 
 export const POST = withValidation<RoomRequest>(
   RoomRequestSchema,
-  ({ body }) => {
-    console.log(body);
-    return NextResponse.json(body);
+  async ({ body }) => {
+    try {
+      const createdRoom = await db.room.create({
+        data: body,
+      });
+      return NextResponse.json(createdRoom, { status: 201 });
+    } catch (error) {
+      return NextResponse.json(error, {
+        status: 500,
+      });
+    }
   }
 );
